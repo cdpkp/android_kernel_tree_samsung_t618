9487af38e2a1c2a68df36c348bddb8a67d2b91b0
Carlo Dee <carlodee.official@proton.me>
2024-05-07 00:46:07 +0000

    build_kernel: Added 'cpio' package and define LD_LIBRARY_PATH.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- build_kernel.sh
+++ build_kernel.sh
@@ -15,13 +15,14 @@ fi
 # Install Packages (In case your server don't have this pre-installed)
 # Run `sudo apt-get update -y` as well.
 # sudo apt-get update -y
-sudo apt-get install bison flex rsync bison device-tree-compiler bc -y
+sudo apt-get install bison flex rsync bison device-tree-compiler bc cpio -y
 
 # Exports
 export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
 export CLANG_TOOL_PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/
 export PATH=${CLANG_TOOL_PATH}:${PATH//"${CLANG_TOOL_PATH}:"}
+export LD_LIBRARY_PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/lib64
 export BSP_BUILD_FAMILY=qogirl6
 export DTC_OVERLAY_TEST_EXT=$(pwd)/tools/mkdtimg/ufdt_apply_overlay
 export DTC_OVERLAY_VTS_EXT=$(pwd)/tools/mkdtimg/ufdt_verify_overlay_host
